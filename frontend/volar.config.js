fc1312ade7653da1ed3dc0509d14fec18193b9ca
jonathanroll <75393433+jonathanroll@users.noreply.github.com>
2022-04-22 18:02:51 +0000

    chore(core): rebuilding repo

--- frontend/volar.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    plugins: [
-        require('@volar-plugins/prettier-html').default({ printWidth: 100 }),
-    ],
-};
--- tmp/backend/config/admin.js
+++
@@ -1,5 +0,0 @@
-module.exports = ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET', '6121bfab7e8410704bb7a87dedf1e9d7'),
-  },
-});
