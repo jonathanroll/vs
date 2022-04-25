ca62c49f6b771958d9b6a121d995bb900a6ece2a
jonathanroll <75393433+jonathanroll@users.noreply.github.com>
2022-04-25 20:17:55 +0000

    initial commit ex5

---
+++ v3compositionApi/reactivity-examples/ex5/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref, reactive } from 'vue'
+
+const state = reactive({count: 0, iLable: 'Increment'})
+const increment = () => state.count++
+const decrement = () => state.count--
+</script>
+
+<template>
+<h1>{{ state.count }}</h1>
+<!-- <button @click="state.count++"> -->
+<button @click="increment">
+{{ state.iLable }}
+</button>
+<button @click="decrement">
+Decrement
+</button>
+</template>
